703906464a3b6c62339537a8594ccb0aeca2a11b
David Bautista <davidbaar07@gmail.com>
2025-11-29 22:39:27 -0600

    feat(genres) create module and CRUD of genres

--- apps/genres/views.py
+++ apps/genres/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Genre
+from .serializers import GenreSerializer, GenreListSerializer
+from utils.mixins import MultipleSerializerMixin
+
+
+class GenreListCreateView(MultipleSerializerMixin, ListCreateAPIView):
+    queryset = Genre.objects.filter(is_active=True)
+    serializer_class = GenreSerializer
+    serializer_list = GenreListSerializer
+
+
+class GenreRetrieveUpdateDestroyView(MultipleSerializerMixin, RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.filter(is_active=True)
+    serializer_class = GenreSerializer
+    serializer_list = GenreListSerializer
+    lookup_field = 'id'
+
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.soft_delete()
+        return Response({'message': 'GÃ©nero eliminado correctamente'}, status=status.HTTP_200_OK)
